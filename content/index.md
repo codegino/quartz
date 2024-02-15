6655f4033196bee75fe07afceecc23b7893704cc
codegino <carloginocatapang@gmail.com>
2024-02-15 15:05:03 +0100

    Sync content

--- content/index.md
+++ content/index.md
@@ -3,6 +3,8 @@
 title: Welcome to My Digital Garden
 
 ---
+
+
 This is a blank Quartz installation.
 
 See the [documentation](https://quartz.jzhao.xyz) for how to get started.
--- publish-content.sh
+++ publish-content.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Run the sync-content.sh script
+# Run the sync-content.sh script which is outside of this repo
 ../sync-content.sh
+wait
 
 # Add all changes to the staging area
-git add .
+git add content
+
+# Check if there are any changes
+if [[ -z $(git status --porcelain) ]]; then
+  echo "No changes to commit. Exiting..."
+  exit 0
+fi
 
 # Commit the changes
 git commit -m "Sync content"
 
-# # Push the changes to the remote repository
-# git push
+# Push the changes to the remote repository
+git push
